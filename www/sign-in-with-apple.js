d17c61423603e45e9ca8a98e115d3e89d7474bb5
Dmitry Borisov <pnegebodyguard@gmail.com>
2021-03-11 11:59:51 +0300

    minor changes

--- www/sign-in-with-apple.js
+++ www/sign-in-with-apple.js
@@ -1,5 +1,3 @@
-var exec = require('cordova/exec');
-
 exports.signin = function(arg0, success, error) {
-  exec(success, error, "SignInWithApple", "signin", [arg0]);
+  window.cordova.exec(success, error, "SignInWithApple", "signin", [arg0]);
 };
